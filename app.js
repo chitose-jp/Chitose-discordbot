df9920b8f0ec8cb56a4b3b1bec08c0a53eb1020b
ちとせあめ <chitoseame.jp@gmail.com>
2025-03-24 23:00:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const {Client,Events,GatewayIntentBits}=require("discord.js");
+const token=process.env.TOKEN;
+
+const client=new Client({intents:[GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, c => {
+	console.log(`準備OKです! ${c.user.tag}がログインします。`);
+});
+
+client.login(token);
 
