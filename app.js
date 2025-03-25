cc498bc4e0d2aedfcd553307de8cc717297a334b
ちとせあめ <chitoseame.jp@gmail.com>
2025-03-25 19:23:24 +0900

    ヘルスチェック対策を追加

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ client.once(Events.ClientReady,c=>{
 });
 
 client.login(token);
+
+//Koyebの仕様でhttpサーバーを立てないといけないので、expressを利用する
+const express=require("express");
+
+const app=express();
+
+app.get("/",()=>{res.send("Server is running.")});
+
+const port=process.env.PORT||8080;
+app.listen(port,()=>{console.log('Server is running on PORT ${port}.')});
