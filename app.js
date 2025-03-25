b212df54e5443d12b008f4e62626dd63d4e3a0d7
ちとせあめ <chitoseame.jp@gmail.com>
2025-03-25 20:44:50 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const {Client,Events,GatewayIntentBits}=require("discord.js");
 const token=process.env.TOKEN;
 
-const client=new Client({intents:[GatewayIntentBits.Guilds]});
+const client=new Client({intents:[
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.MessageContent
+]});
 
 client.once(Events.ClientReady,c=>{
 	console.log(`準備OKです! ${c.user.tag}がログインします。`);
 });
 
 client.on("messageCreate",message=>{
-	console.log(JSON.stringify(message));
+	console.log(`受信しました${JSON.stringify(message)}`);
 });
 
 client.login(token);
@@ -18,7 +21,7 @@ const express=require("express");
 
 const app=express();
 
-app.get("/",()=>{res.sendStatus(200)});
+app.get("/",(req,res)=>{res.sendStatus(200)});
 
 const port=process.env.PORT||8080;
 app.listen(port,()=>{console.log(`Server is running on PORT ${port}.`)});
