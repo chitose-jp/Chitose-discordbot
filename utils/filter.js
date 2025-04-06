c62a739de0a7a33719d99cad1a68f5fbfb90c994
ちとせあめ <chitoseame.jp@gmail.com>
2025-04-06 10:22:20 +0900

    Update filter.js

--- utils/filter.js
+++ utils/filter.js
@@ -1,1 +1,11 @@
+const bannedWords=[];
 
+const reg=new RegExp(`(?:${bannedWords.jpin("|")})`);
+
+function checkBannedWords(str){
+  const match=String(str).match(reg);
+  if(match)return true;
+  else return false;
+}
+
+module.exports=checkBannedWords;
